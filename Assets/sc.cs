6efd5f8809dbdd6136e9bc10f2e4d81f981ba1dd
sofieg <sofiegonzalez98@gmail.com>
2018-11-15 19:09:29 -0500

    monument_penrose

---
+++ Assets/sc.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sc : MonoBehaviour {
+
+    private bool rotatingClockwise = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (rotatingClockwise && (transform.eulerAngles.z > 270 || transform.eulerAngles.z <= 0))
+		{
+			transform.Rotate(0, 1, 0);
+		}
+		else if (!rotatingClockwise && transform.eulerAngles.z > 0)
+		{
+			transform.Rotate(0, -1, 0);
+
+		}
+
+		if (Input.anyKeyDown)
+		{
+			rotatingClockwise = !rotatingClockwise;
+		}
+	}
+}
